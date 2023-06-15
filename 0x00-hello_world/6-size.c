a629ca00d46bb7ccca272db84df8eed79276b76a
Ramoun <omartarekramoun@gmail.com>
2023-06-16 03:37:37 +0500

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,10 +3,11 @@
 /**
  * main - Entry point
  *
- * Des: Print size of data types
+ * Description: Print the size of various data types
  *
- * Return: Always 0 (SUCCESS)
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	printf("Size of a char: %ld byte(s)\n", sizeof(char));
@@ -14,5 +15,6 @@ int main(void)
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+
 	return (0);
 }

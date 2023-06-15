db59314a7bf2e304fff27daf68ab22fc497244d9
OmarRamoun <omartarekramoun@gmail.com>
2023-06-15 15:21:26 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * main - diaplay "Programming is like bulding a multilingual puzzle"
+ * return 0 and exit
+ * */
+int main(void)
+{
+    puts("\"Programming is like bulding a multilingual puzzle");
+    return (0);
+}

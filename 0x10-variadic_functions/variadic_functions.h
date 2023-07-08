7d284a53c38009b3ead8340e431baa55620cb363
Ramoun <omartarekramoun@gmail.com>
2023-07-08 13:40:57 +0500

    Update variadic_functions.h

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef MYVARFUNC_H
+#define MYVARFUNC_H
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif

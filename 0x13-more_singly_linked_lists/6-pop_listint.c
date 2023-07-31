b8db072e6d42a58e5018123d6f5b2afb5d8616ca
Ramoun <omartarekramoun@gmail.com>
2023-07-31 08:21:13 +0500

    Update 6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - deqwgqwfwqdwqdwqde
+ * @head: awqdqwdwqdqwder
+ *Return: emwqdqwdwqdpty
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *start;
+	int n;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+	start = *head;
+	*head = start->next;
+	n = start->n;
+	free(start);
+	return (n);
+}

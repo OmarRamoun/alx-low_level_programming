a5a8989acf36eb8dec743209671fb308717fa1a1
Ramoun <omartarekramoun@gmail.com>
2023-07-31 08:21:29 +0500

    Update 7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - tqwdwqfqwregwegefwqeft.
+ * @head: poiwerhgwrefwasdwqdfqwefge
+ * @index: irethertgedqwqwde
+ * Return: wqdqwafqegegewrgrehgrwgh
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+	for (i = 0; i < index; i++)
+	{
+		head = head->next;
+		if (head == NULL)
+			return (NULL);
+	}
+	return (head);
+}

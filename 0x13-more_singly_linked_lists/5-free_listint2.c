30d88991a0ba7b24caf6a93b7f59f00fc9b61400
Ramoun <omartarekramoun@gmail.com>
2023-07-31 08:20:56 +0500

    Update 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - fqwdweqgrwegrwegrehertht
+ * @head: qwdweqgreghergwergfs
+ * Return: Just nothing.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *next;
+
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+}
